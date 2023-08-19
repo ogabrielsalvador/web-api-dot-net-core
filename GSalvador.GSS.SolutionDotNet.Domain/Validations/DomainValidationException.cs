554be177d1214fef340fd78fa409947d55c9f396
Gabriel Salvador <102549645+ogabrielsalvador@users.noreply.github.com>
2023-08-18 22:38:46 -0300

    feat: add a custom exception

---
+++ GSalvador.GSS.SolutionDotNet.Domain/Validations/DomainValidationException.cs
@@ -0,0 +1,12 @@
+namespace GSalvador.GSS.SolutionDotNet.Domain.Validations;
+
+public class DomainValidationException : Exception
+{
+    public DomainValidationException(string error) : base(error) { }
+
+    public static void When(bool hasError, string message)
+    {
+        if (hasError)
+            throw new DomainValidationException(message);
+    }
+}
