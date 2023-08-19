13bd246310832e727b29d25e087f4548405f6db5
Gabriel Salvador <102549645+ogabrielsalvador@users.noreply.github.com>
2023-08-19 19:50:26 -0300

    feat: add database connection context

---
+++ GSalvador.GSS.SolutionDotNet.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace GSalvador.GSS.SolutionDotNet.Infra.Data.Context;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
+}
